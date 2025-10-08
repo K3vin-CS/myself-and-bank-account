47c705d1bcba031fad08779685feb883483955f2
K3vin-CS <kevin.kosuma@binus.ac.id>
2025-10-08 11:37:15 +0700

    added new file

--- myself.py
+++ myself.py
@@ -2,14 +2,14 @@ class Human:
     def __init__(self, name, age, hobby, school):
         self.name = name
         self.age = age
-        self.hobby = hobby
+        self.hobby= hobby
         self.school = school
     
     def introduce(self):
         return f"Hi my name is {self.name}. I am {self.age} years old and I study at {self.school}"
 
     def do_hobby(self):
-        return "My hobby is {self.hobby}"
+        return f"My hobby is {self.hobby}"
 
     def goal(self, dream_job):
         return f"My name is {self.name} and my biggest dream is to be a {dream_job} "
@@ -17,6 +17,6 @@ class Human:
 bro = Human("Kevin", 18, "playing games", "binus university")
 
 print(bro.introduce())
-print(bro.do_hobby)
+print(bro.do_hobby())
 print(bro.goal("Software Engineer"))
 
